e68f65288b8c05a42e3cb19a0944b521164b6206
小貫将太 <onuonu007@gmail.com>
2018-06-09 12:32:56 +0900

    [Update]プロフィールページ修正

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -4,6 +4,10 @@
     <div class="container">
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
+                <div class="text-center">
+                    <h1 style="color: #d62728; font-size: 96px;">Wonderbase</h1>
+                    <h3>誰もがわくわくするプロジェクトを持っている世界。</h3>
+                </div>
             </div>
         </div>
     </div>
--- resources/views/users/profile.blade.php
+++ resources/views/users/profile.blade.php
@@ -3,8 +3,20 @@
 @section('content')
     <div class="container">
         <div class="row">
-            <div class="col-md-8 col-md-offset-2">
-                {{$user->id}}
+            <div class="text-center">
+                <img src="{{ asset('images/users/no-profilepic.png') }}" class="profile-image" alt="プロフィール写真" width="100px">
+                <div>
+                    {!! link_to('users/profile/edit/'.Auth::user()->id, 'プロフィールを編集する') !!}
+                </div>
+                <h4>{{$user->name}}</h4>
+            </div>
+            <div class="text-center">
+                <section id="self-introduction">
+                    @isset($user->self_introduction)
+                        <h4>自己紹介</h4>
+                        <p>{{$user->self_introduction}}</p>
+                    @endisset
+                </section>
             </div>
         </div>
     </div>
