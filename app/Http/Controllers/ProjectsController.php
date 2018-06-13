a05343e5a24b03291073f6bed2135ead179cbaff
小貫将太 <onuonu007@gmail.com>
2018-06-13 19:02:43 +0900

    [Update]プロジェクト保存アクションのみ実装

--- app/Http/Controllers/ProjectsController.php
+++ app/Http/Controllers/ProjectsController.php
@@ -31,6 +31,29 @@ class ProjectsController extends Controller
         } else {
             return redirect('/login');
         }
+    }
 
+    public function storeProject(Request $request)
+    {
+        $this->validate($request,
+            [
+                'title' => 'required|min:10',
+                'main_image' => 'required',
+                'summary' => 'required',
+                'category' => 'required',
+                'occupation' => 'required',
+                'description' => 'required',
+            ]);
+        $projects = new Project;
+        $projects->user_id = Auth::user()->id;
+        $projects->title = $request->title;
+        $projects->main_image = $request->main_image;
+        $projects->summary = $request->title;
+        $projects->category = $request->title;
+        $projects->occupation = $request->title;
+        $projects->description = $request->title;
+        $projects->is_published = false;
+        $projects->save();
+        return redirect('/');
     }
 }
