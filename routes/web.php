1f72959e7a85422650b1962652515f29d3df40a2
小貫将太 <onuonu007@gmail.com>
2018-06-14 20:01:54 +0900

    [Update]ルーティング修正

--- routes/web.php
+++ routes/web.php
@@ -18,12 +18,12 @@ Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('/', ['as' => 'projects.index', 'uses' => 'ProjectsController@index']);
 Route::get('/projects/gettingstarted', ['as' => 'projects.gettingStarted', 'uses' => 'ProjectsController@gettingStarted']);
-Route::get('/projects/{id}', ['as' => 'projects.detail', 'uses' => 'ProjectsController@detail']);
 Route::get('/projects/form', ['as' => 'projects.form', 'uses' => 'ProjectsController@form']);
 Route::post('/projects/form/complete', 'ProjectsController@formComplete');
+Route::get('/projects/{id}', ['as' => 'projects.detail', 'uses' => 'ProjectsController@detail']);
 
-Route::get('/users/profile/{id}', ['as' => 'users.profile', 'uses' => 'UsersController@profile']);
 Route::get('/users/edit', ['as' => 'users.edit', 'uses' => 'UsersController@edit']);
 Route::post('/users/edit/complete', ['as' => 'users.editComplete', 'uses' => 'UsersController@editComplete']);
-Route::get('/users/user_information', ['as' => 'users.user_information', 'uses' => 'UsersController@userInformation']);
+Route::patch('/users/user_information', ['as' => 'users.user_information', 'uses' => 'UsersController@userInformation']);
 Route::post('/users/user_information/complete', 'UsersController@userInformationComplete');
+Route::get('/users/profile/{id}', ['as' => 'users.profile', 'uses' => 'UsersController@profile']);
